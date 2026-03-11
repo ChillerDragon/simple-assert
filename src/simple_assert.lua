ee8c792c5720e169f3126f4f4a1f96bdd57ce6c7
ChillerDragon <chillerdragon@gmail.com>
2026-03-11 11:23:23 +0100

    Simple assert_eq

---
+++ src/simple_assert.lua
@@ -0,0 +1,17 @@
+local function assert_eq(expected, actual)
+	if expected == actual then
+		return
+	end
+
+	local msg = string.format([[
+❌ Assertion failed
+  Expected: %s
+  Actual:   %s
+]], tostring(expected), tostring(actual))
+
+	error(msg, 2)
+end
+
+return {
+	assert_eq = assert_eq
+}
