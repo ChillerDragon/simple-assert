62b0bd6f355da13e032026f2a618b15b4d809a9b
ChillerDragon <chillerdragon@gmail.com>
2026-04-13 14:10:29 +0200

    Add assert_ne
    Closed #1

--- src/simple_assert.lua
+++ src/simple_assert.lua
@@ -12,6 +12,21 @@ local function assert_eq(expected, actual)
 	error(msg, 2)
 end
 
+local function assert_ne(not_expected, actual)
+	if not_expected ~= actual then
+		return
+	end
+
+	local msg = string.format([[
+❌ Assertion failed
+  Expected: '%s' != '%s'
+  Actual:   '%s' == '%s'
+]], tostring(not_expected), tostring(actual), tostring(not_expected), tostring(actual))
+
+	error(msg, 2)
+end
+
 return {
-	assert_eq = assert_eq
+	assert_eq = assert_eq,
+	assert_ne = assert_ne,
 }
