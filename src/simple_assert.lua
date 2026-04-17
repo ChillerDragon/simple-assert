81c9a20cdaeabf4978c76c3a25fb230cbeb301ff
ChillerDragon <chillerdragon@gmail.com>
2026-04-17 11:36:50 +0200

    Add assert_str_match

--- src/simple_assert.lua
+++ src/simple_assert.lua
@@ -26,7 +26,22 @@ local function assert_ne(not_expected, actual)
 	error(msg, 2)
 end
 
+local function assert_str_match(pattern, text)
+	if string.match(text, pattern) then
+		return
+	end
+
+	local msg = string.format([[
+❌ Assertion failed
+  Expected: '%s'
+  To match: '%s'
+]], tostring(text), tostring(pattern))
+
+	error(msg, 2)
+end
+
 return {
 	assert_eq = assert_eq,
 	assert_ne = assert_ne,
+	assert_str_match = assert_str_match,
 }
